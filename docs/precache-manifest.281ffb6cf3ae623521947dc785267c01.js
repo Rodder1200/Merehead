a326b9dccc24cbf474a58d1837e2a005a329a901
Rodder1200 <denegnet@mail.com>
2018-12-19 16:42:14 +0200

    hosted

---
+++ docs/precache-manifest.281ffb6cf3ae623521947dc785267c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf4188e42f506a584660",
+    "url": "/Merehead/static/js/runtime~main.cf4188e4.js"
+  },
+  {
+    "revision": "8f8dbdc30c517621e442",
+    "url": "/Merehead/static/js/main.8f8dbdc3.chunk.js"
+  },
+  {
+    "revision": "ae76f9d542292f560f73",
+    "url": "/Merehead/static/js/1.ae76f9d5.chunk.js"
+  },
+  {
+    "revision": "8f8dbdc30c517621e442",
+    "url": "/Merehead/static/css/main.a1479d69.chunk.css"
+  },
+  {
+    "revision": "ae76f9d542292f560f73",
+    "url": "/Merehead/static/css/1.975abf9e.chunk.css"
+  },
+  {
+    "revision": "8535a7541e392acc0f9ddf29b30459bb",
+    "url": "/Merehead/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc7f2b38772bfc067624afd0129e2c64.js"
+  "/Merehead/precache-manifest.281ffb6cf3ae623521947dc785267c01.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Merehead/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
